ad3fbbfe29025019f3bdae34be62e018100638d6
cgonzalezt <cristipu123@gmail.com>
2023-10-28 16:18:35 -0400

    Starting to set up Login.js
    Still need to add axios stuff :rage3:

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,85 @@
-import React from 'react'; 
+import {useRef,useState,useEffect} from 'react'; 
+
   
 function Login (){ 
-    return <h1>Login</h1> 
+    //set focus on first input
+    const userRef = useRef();
+    //set focus on errors
+    const errRef = useRef();
+
+    const[user, setUser] = useState('');
+    const[pwd, setPwd] = useState('');
+    const[errMsg, setErrMsg] = useState('');
+    //replace later: routing stage
+    const[success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user,pwd])
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        //debugging
+        console.log(user,pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+    return(
+        // jsx checking succcess
+         <>
+         {success ? (
+            <div>
+                <h1>You are Logged in!</h1>
+                <br />
+                <p>
+                    <a href="#">Go to Home</a>
+                </p>
+            </div>
+         ) : ( 
+        <div>
+            {/* screen reader announces error */}
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Welcome to RevMixer</h1>
+            <h2>Log in</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor ="username">Username:</label>
+                <input 
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    onChange={(e)=>setUser(e.target.value)}
+                    //control input: clear upon submission
+                    value={user}
+                    required
+                />
+
+                <label htmlFor ="password">Password:</label>
+                <input 
+                    type="password"
+                    id="password"
+                    onChange={(e)=>setPwd(e.target.value)}
+                    //control input: clear upon submission
+                    value={pwd}
+                    required
+                />  
+                <button>Log In</button>
+            </form>
+            <p>
+                Don't have an account?<br />
+                <span className="line">
+                    <a href="/register">Register</a>
+                </span>
+            </p>
+        </div>
+         )}
+         </>
+    )
 } 
   
 export default Login;
