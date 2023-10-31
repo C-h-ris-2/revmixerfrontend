d6160753a625d6d93e263ae14d81e1f36b384f61
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-10-31 17:04:15 -0400

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -7,4 +7,57 @@ function RegistrationForm() {
       password: '',
     });
   
-export default Register;
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Make a POST request to your API to register the user
+      axios
+        .post('http://localhost:8080/comp333-hw3-frontend/index.php/user/create', formData, {withCredentials: true})
+        .then((response) => {
+          console.log('User registered:', response.data);
+          // You can redirect to a login page or display a success message here
+        })
+        .catch((error) => {
+          console.error('Registration error:', error);
+          // Handle the registration error here
+        });
+    };
+  
+    return (
+      <div>
+        <h1>User Registration</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationForm;
