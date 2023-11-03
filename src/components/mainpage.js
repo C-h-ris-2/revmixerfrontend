cfa561f4f597d49419d53079ce6bab52552c0c27
cgonzalezt <cristipu123@gmail.com>
2023-11-03 04:06:53 -0400

    added stars functionality + elimiated view

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,14 +1,34 @@
 import React, {useState} from 'react'; 
 import {Link} from "react-router-dom";
-import { FaPlusCircle, FaEdit, FaGlasses, FaTrashAlt } from 'react-icons/fa';
+import { FaPlusCircle, FaEdit, FaGlasses, FaTrashAlt, FaRegStar,FaStar } from 'react-icons/fa';
 import axios from "axios";
 import './mainpage.css';
   
+function Stars(props){
+    const starNum = props.id;
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+        if (i <= starNum) {
+          stars.push(<FaStar key={i} color="gold"/>); // Filled star icon
+        } else {
+          stars.push(<FaRegStar key={i} color="gold"/>); // Empty star icon
+        }
+    }
+    return <span>
+        {stars}
+    </span>
+}
+
 function MainPage (){ 
     const [posts, setPosts] = useState([]);
+        
+    let icons = { color: "white", padding: 20 , background: '#282652'};
+    let icons2 = { color: "white", padding: 20 , background: '#282652'};
+    let spacing = '20px'
+
 
     const query = async () => {
-        let response = await axios.get("http://localhost:8080/comp333-hw3-frontend/index.php/user/songlist", {});
+        let response = await axios.get("http://localhost/comp333hw3/index.php/user/songlist", {});
         if (response.data.code === 0) {
             setPosts(response.data.data);
         } else {
@@ -19,19 +39,11 @@ function MainPage (){
 
     query();
 
-    // const imageClick = async(e) =>{
-    //     console.log('success')
-    // }
-    
-    let icons = { color: "white", padding: 20 , background: '#282652'};
-    let icons2 = { color: "white", padding: 20 , background: '#282652'};
-    let spacing = '20px'
-
     return (
         <div className="mainpageV">
             <p>You are logged in as {localStorage.getItem('username')}</p>
             <h1>Rev Mixer</h1>
-            <Link to="/addnewsong"><FaPlusCircle style={icons2}/> Add a new song!</Link>
+            <Link to="/addnewsong"><FaPlusCircle padding="20px"/> Add a new song!</Link>
             <table>
                         <thead>
                             <tr>
@@ -51,17 +63,12 @@ function MainPage (){
                             <td>{r.username}</td>
                             <td>{r.artist}</td>
                             <td>{r.song}</td>
-                            <td>{r.rating}</td>
                             <td>
-                            <Link 
-                                to={{
-                                        pathname: '/view', 
-                                        state:"hello"
-                                    }}>
-                                    <FaGlasses style={icons}/>
-                                </Link>
-                                <Link to="/update"><FaEdit style={icons}/></Link>
-                                <Link to="/delete"><FaTrashAlt style={icons}/></Link>
+                                <Stars id={r.rating}/>
+                            </td>
+                            <td>
+                                <Link to="/update"><FaEdit color="white" padding="20px" background="pink"/></Link>
+                                <Link to="/delete"><FaTrashAlt  margin-left= "20px"  color="white" padding="20px" background="pink"/></Link>
                             </td>
                         </tr>
                     ))}
