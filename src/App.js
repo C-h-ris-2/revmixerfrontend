16d8244332c9140b0ae6f2209384abd26750da03
cdiazherrera <cdiazherrera@wesleyan.edu>
2023-11-03 04:14:51 -0400

    Revert "Merge branch 'issue7'"
    This reverts commit 4bd1061e2e85259ceb1ad5da521347f1bf62207a, reversing
    changes made to 229ad3a6e807be6001dda49ffd966e8e2070ee55.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
-import MainPage from "./components/mainpage";
-import AddNewSong from "./components/addnewsong";
-import "./mainpage.css";
+import logo from './logo.svg';
+import './App.css';
 
-class  App extends Component {
-  render() {
-      return (
-          <div className="App">
-            <h1 className="title">RevMixer</h1>
-            <MainPage />
-            <AddNewSong/>
-          </div>
-      );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
